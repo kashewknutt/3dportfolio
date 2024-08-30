6b4f29509c23d148c96d2ecb9daa813bc30322ca
Rajat Disawal <kashewknutt@gmail.com>
2024-08-31 01:38:55 +0530

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        primary: "#FFDDC2",
+        secondary: "#FBF6F0",
+        tertiary: "#c87d3b",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
